c10b38f7b523f2fc71a07ca663af481181d26ca3
ghcdg <2538252675@qq.com>
2024-05-08 00:47:11 +0800

    show random images in two ways

--- src/components/ImageBoard.vue
+++ src/components/ImageBoard.vue
@@ -14,25 +14,58 @@ export default {
   },
   data() {
     return {
-      images: [
-        { src: '/src/assets/images/1.jpg', alt: '示例图片1', width: '100px', height: '100px', left: '10px', top: '100px' },
-        { src: '/src/assets/images/2.jpg', alt: '示例图片2', width: '100px', height: '100px', left: '10px', top: '100px' },
-        { src: '/src/assets/images/3.jpg', alt: '示例图片3', width: '100px', height: '100px', left: '10px', top: '100px' },
-        { src: '/src/assets/images/4.jpg', alt: '示例图片4', width: '100px', height: '100px', left: '10px', top: '100px' },
-        { src: '/src/assets/images/5.jpg', alt: '示例图片5', width: '100px', height: '100px', left: '10px', top: '100px' }
-      ]
+      images: []
     };
   },
-  mounted() {
-    this.images.forEach(image => {
-      const randomLeft = Math.floor(Math.random() * (window.innerWidth - 100)); // Random left position within window width
-      const randomTop = Math.floor(Math.random() * (window.innerHeight - 100)); // Random top position within window height
-      image.left = `${randomLeft}px`;
-      image.top = `${randomTop}px`;
-    });
+  created() {
+    const list = this.getDigits(10);
+
+    this.images = list.map((path, index) => ({
+      src: `/src/assets/images/${path}.jpg`,
+      alt: `Image ${index + 1}`,
+      width: '100px',
+      height: '100px',
+      left: `${Math.floor(Math.random() * (window.innerWidth - 100))}px`,
+      top: `${Math.floor(Math.random() * (window.innerHeight - 100))}px`
+    }));
+  },
+  methods: {
+    getDigits: (n) => {
+      return Array.from({ length: n }, (_, index) => index + 1);
+    }
   }
 };
 </script>
+
+<!-- <script>
+import ImageCard from './ImageCard.vue';
+
+export default {
+  name: 'ImageBoard',
+  components: {
+    ImageCard
+  },
+  computed: {
+    images() {
+      const list = this.getDigits(10);
+
+      return list.map((path, index) => ({
+        src: `/src/assets/images/${path}.jpg`,
+        alt: `Image ${index + 1}`,
+        width: '100px',
+        height: '100px',
+        left: `${Math.floor(Math.random() * (window.innerWidth - 100))}px`,
+        top: `${Math.floor(Math.random() * (window.innerHeight - 100))}px`
+      }));
+    }
+  },
+  methods: {
+    getDigits: (n) => {
+      return Array.from({ length: n }, (_, index) => index + 1);
+    }
+  }
+};
+</script> -->
 
 <style scoped>
 .image-board {

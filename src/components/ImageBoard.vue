2081bd88198090ed6c2db04ccf4f5af7b2f16fbf
ghcdg <2538252675@qq.com>
2024-05-09 00:29:03 +0800

    swap image success

--- src/components/ImageBoard.vue
+++ src/components/ImageBoard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="image-board">
-    <ImageCard v-for="(image, index) in images" :key="index" :image="image" @click="moveImage(index, $event)" :class="{ 'image-move': image.move }" />
+    <ImageCard v-for="(image, index) in images" :key="index" :image="image" @click="swapImages(index)" />
   </div>
 </template>
 
@@ -30,13 +30,26 @@ export default {
         width: '100px',
         height: '100px',
         left: `${Math.floor(Math.random() * (window.innerWidth - 100))}px`,
-        top: `${Math.floor(Math.random() * (window.innerHeight - 100))}px`,
-        move: false // 添加一个属性来控制是否添加.image-move类
+        top: `${Math.floor(Math.random() * (window.innerHeight - 100))}px`
       }));
     },
-    moveImage(index, event) {
-      this.images[index].top = `${parseInt(this.images[index].top) - 100}px`;
-      this.images[index].move = true; // 点击后设置move为true
+    getRandomIndex(currentIndex, length) {
+      let randomIndex = Math.floor(Math.random() * length);
+      while (randomIndex === currentIndex) {
+        randomIndex = Math.floor(Math.random() * length);
+      }
+      return randomIndex;
+    },
+    swapImages(index) {
+      const targetIndex = this.getRandomIndex(index, this.images.length);
+      const tempTop = this.images[index].top;
+      const tempLeft = this.images[index].left;
+
+      this.images[index].top = this.images[targetIndex].top;
+      this.images[index].left = this.images[targetIndex].left;
+
+      this.images[targetIndex].top = tempTop;
+      this.images[targetIndex].left = tempLeft;
     }
   }
 };
@@ -49,9 +62,5 @@ export default {
   height: 100%;
   justify-content: center;
   align-items: center;
-}
-
-.image-move {
-  transition: all 0.618s ease;
 }
 </style>

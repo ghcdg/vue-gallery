066920e90eb1105fb91b27fd5816c10441889af8
ghcdg <2538252675@qq.com>
2024-05-10 00:20:47 +0800

    interface divide into 3 area and all image can swap with each other

--- src/components/ImageBoard.vue
+++ src/components/ImageBoard.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="image-board">
-    <ImageCard v-for="(image, index) in images" :key="index" :image="image" @click="swapImages(index)" :style="{ top: image.top, left: image.left }" />
+
+      <ImageCard v-for="(image, index) in leftImages" :key="index" :image="image" @click="swapImages(image.id)" :style="{ top: image.top, left: image.left }" />
+      <ImageCard v-if="middleImage" :image="middleImage" @click="swapImages(middleImage.id)" :style="{ top: middleImage.top, left: middleImage.left }" />
+      <ImageCard v-for="(image, index) in rightImages" :key="index" :image="image" @click="swapImages(image.id)" :style="{ top: image.top, left: image.left }" />
   </div>
 </template>
 
@@ -15,28 +18,67 @@ export default {
   data() {
     return {
       images: [],
+      leftImages: [],
+      middleImage: null,
+      rightImages: [],
       defaultWidth: 100,
       defaultHeight: 100,
+      leftAreaWidth: window.innerWidth * 0.4,
+      centerAreaWidth: window.innerWidth * 0.2,
+      rightAreaWidth: window.innerWidth * 0.4
     };
   },
   mounted() {
     this.images = this.generateImages();
-      // 将第一张图片放在界面的正中间
-      this.images[0].top = `${(window.innerHeight - this.defaultHeight) / 2}px`;
-      this.images[0].left = `${(window.innerWidth - this.defaultWidth) / 2}px`;
+    this.leftImages = this.images.filter(image => image.area === 'left');
+    this.middleImage = this.images.find(image => image.area === 'middle');
+    this.rightImages = this.images.filter(image => image.area === 'right');
   },
   methods: {
     generateImages() {
       const list = Array.from({ length: 15 }, (_, index) => index + 1);
-      return list.map((path, index) => ({
-        id: index,
-        src: `/src/assets/images/${path}.jpg`,
-        alt: `Image ${index + 1}`,
-        width: `${this.defaultWidth}px`,
-        height: `${this.defaultHeight}px`,
-        left: `${Math.floor(Math.random() * (window.innerWidth - this.defaultWidth))}px`,
-        top: `${Math.floor(Math.random() * (window.innerHeight - this.defaultHeight))}px`
-      }));
+      return list.map((path, index) => {
+        let area;
+        if (index === 0) {
+          area = 'middle';
+        } else {
+          area = index % 2 === 0 ? 'left' : 'right';
+        }
+        let left;
+        if (area === 'left') {
+          left = `${Math.floor(Math.random() * (this.leftAreaWidth - this.defaultWidth))}px`;
+        } else if (area === 'right') {
+          left = `${this.leftAreaWidth + this.centerAreaWidth + Math.floor(Math.random() * (this.rightAreaWidth - this.defaultWidth))}px`;
+        } else {
+          left = `${(window.innerWidth - this.defaultWidth) / 2}px`;
+        }
+        return {
+          id: index,
+          src: `/src/assets/images/${path}.jpg`,
+          alt: `Image ${index + 1}`,
+          width: `${this.defaultWidth}px`,
+          height: `${this.defaultHeight}px`,
+          left: left,
+          top: `${Math.floor(Math.random() * (window.innerHeight - this.defaultHeight))}px`,
+          area: area
+        };
+      });
+    },
+    swapImages(imageId) {
+      const imageIndex = this.images.findIndex(image => image.id === imageId);
+      const targetIndex = this.getRandomIndex(imageIndex, this.images.length);
+      const tempTop = this.images[imageIndex].top;
+      const tempLeft = this.images[imageIndex].left;
+
+      this.images[imageIndex].top = this.images[targetIndex].top;
+      this.images[imageIndex].left = this.images[targetIndex].left;
+
+      this.images[targetIndex].top = tempTop;
+      this.images[targetIndex].left = tempLeft;
+
+      this.leftImages = this.images.filter(image => image.area === 'left');
+      this.middleImage = this.images.find(image => image.area === 'middle');
+      this.rightImages = this.images.filter(image => image.area === 'right');
     },
     getRandomIndex(currentIndex, length) {
       let randomIndex = Math.floor(Math.random() * length);
@@ -44,17 +86,6 @@ export default {
         randomIndex = Math.floor(Math.random() * length);
       }
       return randomIndex;
-    },
-    swapImages(index) {
-      const targetIndex = this.getRandomIndex(index, this.images.length);
-      const tempTop = this.images[index].top;
-      const tempLeft = this.images[index].left;
-
-      this.images[index].top = this.images[targetIndex].top;
-      this.images[index].left = this.images[targetIndex].left;
-
-      this.images[targetIndex].top = tempTop;
-      this.images[targetIndex].left = tempLeft;
     }
   }
 };
@@ -62,8 +93,7 @@ export default {
 
 <style scoped>
 .image-board {
-  position: relative;
-  width: 100%;
+  display: flex;
   height: 100%;
 }
 </style>
